224ef82d6683ce5bcfe8a03930669c756c9a4e39
KVBAMusic <kubaokon8@gmail.com>
2024-02-03 15:41:26 +0100

    added inner quarterpipe corner

--- Assets/Scripts/RoadGenerator/Primitives/QuarterPipeGenerator.cs
+++ Assets/Scripts/RoadGenerator/Primitives/QuarterPipeGenerator.cs
@@ -16,6 +16,7 @@ namespace MeshGenRoads {
         [Header("Materials")]
         public List<Material> materials;
         public bool flipUvs;
+        private bool flipNormals = false;
         [Header("Modifier")]
         public SurfaceModifier modifier = new();
 
@@ -30,13 +31,14 @@ namespace MeshGenRoads {
             List<Vector2> uvs = new();
             Mesh mesh = new();
 
-            Vector3 circlePoint = new(0, length, 0);
-            Vector3 circleDir = Vector3.down;
-            Vector3 circleAxis = Vector3.forward;
+            Vector3 circlePoint = new(mode == QuarterpipeMode.Inside ? length : 0, length, 0);
+            Vector3 circleDir = mode == QuarterpipeMode.Inside ? Vector3.back : Vector3.down;
+            Vector3 circleAxis = mode == QuarterpipeMode.Inside ? Vector3.left : Vector3.forward;
             Vector3 currPoint;
 
             float t = 1f / (subdivisions + 1);
             float wt = 1f / (widthSubdivisions + 1);
+
             float radius = length - margin;
 
             float circleAngle = wt * 90f;
@@ -59,17 +61,34 @@ namespace MeshGenRoads {
                     circleAxis = Quaternion.Euler(0, axisAngle, 0) * circleAxis;
                 }
                 circleDir = Vector3.down;
+                if (mode == QuarterpipeMode.Inside) {
+                    circlePoint.x = Mathf.Sin((i + 1) * axisAngle * Mathf.Deg2Rad) * -length + length;
+                    circlePoint.z = Mathf.Cos((i + 1) * axisAngle * Mathf.Deg2Rad) * length - length;
+                    circleAxis = Quaternion.Euler(0, -axisAngle, 0) * circleAxis;
+                    circleDir = (new Vector3(length, length, - length) - circlePoint).normalized;
+                }
 
                 for (int j = 0; j <= widthSubdivisions + 1; j++) {
                     currPoint = circlePoint + circleDir * radius;
-                    circleDir = Quaternion.Euler(circleAxis * circleAngle) * circleDir;
                     verts.Add(currPoint);
                     uvs.Add(new(1 - j * wt, flipUvs ? 1 - (i + 1) * t : (i + 1) * t ));
+                    circleDir = Quaternion.Euler(circleAxis * circleAngle) * circleDir;
                 }
             }
 
             for (int y = 0; y < subdivisions + 1; y++) {
                 for (int x = 0; x < widthSubdivisions + 1; x++) {
+
+                    if (flipNormals) {
+                        inds.Add(y * ((int)widthSubdivisions + 2) + x);
+                        inds.Add(y * ((int)widthSubdivisions + 2) + x + (int)widthSubdivisions + 2);
+                        inds.Add(y * ((int)widthSubdivisions + 2) + x + 1);
+
+                        inds.Add(y * ((int)widthSubdivisions + 2) + x + (int)widthSubdivisions + 3);
+                        inds.Add(y * ((int)widthSubdivisions + 2) + x + 1);
+                        inds.Add(y * ((int)widthSubdivisions + 2) + x + (int)widthSubdivisions + 2);
+                        continue;
+                    }
                     inds.Add(y * ((int)widthSubdivisions + 2) + x);
                     inds.Add(y * ((int)widthSubdivisions + 2) + x + 1);
                     inds.Add(y * ((int)widthSubdivisions + 2) + x + (int)widthSubdivisions + 2);
